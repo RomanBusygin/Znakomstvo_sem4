7e395cb6112795698226646193b3611b9a72b08c
RomanBusygin <roman.alex.95@mail.ru>
2022-07-07 16:03:23 +0500

    изменил тип данных на string для универсальности

--- DZ_sem4/Task 29/Task 29.cs
+++ DZ_sem4/Task 29/Task 29.cs
@@ -1,13 +1,13 @@
 ﻿Console.WriteLine("Введите элементы массива через кнопку 'Enter': ");
-int [] elements = new int [8];
+string [] elements = new string [8];
 
 for (int i = 0; i < elements.Length; i++)
 {
-    elements[i] = Convert.ToInt32(Console.ReadLine());
+    elements[i] = Console.ReadLine();
 }
 PrintArray(elements);
 
-void PrintArray (int [] array)
+void PrintArray (string [] array)
 {
     Console.Write("Вы составили следующий массив: ");
     for (int i = 0; i < array.Length; i++)
