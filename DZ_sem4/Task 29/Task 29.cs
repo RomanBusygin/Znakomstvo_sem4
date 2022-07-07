955e3fbd905e0e11ced5dd078312b4024c92ef4d
RomanBusygin <roman.alex.95@mail.ru>
2022-07-07 15:59:10 +0500

    Готово

---
+++ DZ_sem4/Task 29/Task 29.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите элементы массива через кнопку 'Enter': ");
+int [] elements = new int [8];
+
+for (int i = 0; i < elements.Length; i++)
+{
+    elements[i] = Convert.ToInt32(Console.ReadLine());
+}
+PrintArray(elements);
+
+void PrintArray (int [] array)
+{
+    Console.Write("Вы составили следующий массив: ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
