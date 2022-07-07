17c993cf6b4ba20c6aa0ac95fc198127ab3fa572
RomanBusygin <roman.alex.95@mail.ru>
2022-07-07 15:29:21 +0500

    Задача 27 готова

---
+++ DZ_sem4/Task 27/Task 27.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: "); // 452   123456
+int number = Convert.ToInt32(Console.ReadLine()),
+    sum = 0;
+
+for (int lowDigit = number; lowDigit !=0; lowDigit /=10)
+{
+    sum += lowDigit % 10;
+}
+Console.Write($"Сумма разрядов числа {number} равна: {sum}");
