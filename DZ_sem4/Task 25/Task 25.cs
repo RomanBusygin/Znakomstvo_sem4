ae6eaf9060ae454b1e7648ee23dbc60537824d75
RomanBusygin <roman.alex.95@mail.ru>
2022-07-07 14:09:41 +0500

    Готово

--- DZ_sem4/Task 25/Task 25.cs
+++ DZ_sem4/Task 25/Task 25.cs
@@ -2,4 +2,14 @@
 int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число В: ");
 int b = Convert.ToInt32(Console.ReadLine());
+int result = 1;
 
+if (b != 0)
+{
+    for (int i = 0; i < b; i++)
+    {
+        result = result * a;
+    }
+    Console.Write($"Число {a} в степени {b} равно {result}");
+}
+else Console.Write($"Любое число в степени 0 равно 1");
