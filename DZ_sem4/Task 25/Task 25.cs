a73573e6304f2b90e75ecc68e38e43c2096dd8ff
RomanBusygin <roman.alex.95@mail.ru>
2022-07-07 14:13:06 +0500

    Исправил для работы с отрицательными числами и 0

--- DZ_sem4/Task 25/Task 25.cs
+++ DZ_sem4/Task 25/Task 25.cs
@@ -4,7 +4,7 @@ Console.Write("Введите число В: ");
 int b = Convert.ToInt32(Console.ReadLine());
 int result = 1;
 
-if (b != 0)
+if (b > 0)
 {
     for (int i = 0; i < b; i++)
     {
@@ -12,4 +12,5 @@ if (b != 0)
     }
     Console.Write($"Число {a} в степени {b} равно {result}");
 }
-else Console.Write($"Любое число в степени 0 равно 1");
+else if (b == 0) {Console.Write($"Любое число в степени 0 равно 1");}
+else Console.Write($"Степень не может быть отрицательной");
