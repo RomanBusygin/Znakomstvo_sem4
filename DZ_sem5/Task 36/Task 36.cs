aacb23adbc959e51a11eaba080798b9da0d1d90a
RomanBusygin <roman.alex.95@mail.ru>
2022-07-09 15:31:54 +0500

    36 готова

---
+++ DZ_sem5/Task 36/Task 36.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int [] numbers = new int [size];
+int minValue = -100;
+int maxValue = 101;
+int sum = 0;
+
+InputArray(numbers);
+PrintArray(numbers);
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (i % 2 != 0) sum+= numbers[i];
+}
+Console.Write($"Сумма элементов четных позиций равна {sum}");
+
+void InputArray (int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue);
+    }
+}
+
+void PrintArray(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}  ");
+    }
+    Console.WriteLine();
+}
