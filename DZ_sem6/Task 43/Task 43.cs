27b32591ed9081f3c56fc30fa8b7021ebe097ff5
RomanBusygin <roman.alex.95@mail.ru>
2022-07-11 11:11:33 +0500

    Подозрительно просто

---
+++ DZ_sem6/Task 43/Task 43.cs
@@ -0,0 +1,12 @@
+﻿double b1 = Input_factors("Введите коэффициент b1: ");
+double k1 = Input_factors("Введите коэффициент k1: ");
+double b2 = Input_factors("Введите коэффициент b2: ");
+double k2 = Input_factors("Введите коэффициент k2: ");
+
+Console.Write($"Точка пересечения двух прямых с введенными коэффициентами будет иметь координаты: ({(b2 - b1) / (k1 - k2)};{k1 * (b2 - b1) / (k1 - k2) + b1})");
+
+double Input_factors (string line)
+{
+    Console.Write(line);
+    return Convert.ToDouble(Console.ReadLine());
+}
