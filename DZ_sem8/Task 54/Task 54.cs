297ee37916b3fa83d5e050948d16b589a8f0144e
RomanBusygin <roman.alex.95@mail.ru>
2022-07-23 09:10:45 +0500

    Сортировка по возрастанию

---
+++ DZ_sem8/Task 54/Task 54.cs
@@ -0,0 +1,54 @@
+﻿Random rnd = new Random();
+int row = rnd.Next(3, 7);
+int column = rnd.Next(3, 7);
+int[,] array = new int[row, column];
+
+FillArray(array);
+PrintArray(array);
+SortingRowsToGrow(array);
+PrintArray(array);
+
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(0, 10);
+        }
+    }
+}
+
+void SortingRowsToGrow(int[,] array)
+{
+    int temp = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = j + 1; k < array.GetLength(1); k++)
+            {
+                if (array[i, k] < array[i, j])
+                {
+                    temp = array[i, j];
+                    array[i, j] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
