718eed9ab33702c6f8eaf5d1f498be157c39b4c7
RomanBusygin <roman.alex.95@mail.ru>
2022-07-24 14:44:16 +0500

    Числа от M до N

---
+++ DZ_sem9/Task 64/Task 64.cs
@@ -0,0 +1,28 @@
+﻿int numberM = InputInt("Введите число M: ");
+int numberN = InputInt("Введите число N: ");
+
+Console.Write($"Натуральные числа от {numberM} до {numberN}: ");
+Console.Write(NaturalNumbers(numberM, numberN));
+
+int NaturalNumbers(int m, int n)
+{
+    if (m < n)
+    {
+        if (m == n) return n;
+        Console.Write(NaturalNumbers(m, n - 1) + ", ");
+        return n;
+    }
+    else if (m > n)
+    {
+        if (m == n) return n;
+        Console.Write(NaturalNumbers(m, n + 1) + ", ");
+        return n;
+    }
+    else return m;
+}
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
