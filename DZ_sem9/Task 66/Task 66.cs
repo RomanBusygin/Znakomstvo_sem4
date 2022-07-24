cc72b1e3ed33541cc4f1c1001e4d1c38a5f56b15
RomanBusygin <roman.alex.95@mail.ru>
2022-07-24 14:59:27 +0500

    Сумма элементов

---
+++ DZ_sem9/Task 66/Task 66.cs
@@ -0,0 +1,25 @@
+﻿int numberM = InputInt("Введите число M: ");
+int numberN = InputInt("Введите число N: ");
+
+Console.Write($"Сумма натуральных чисел от {numberM} до {numberN} равна: {SumElements(numberM, numberN)}");
+
+int SumElements(int m, int n)
+{
+    if (m < n)
+    {
+        if (m == n) return n;
+        return m + SumElements(m + 1, n);
+    }
+    if (m > n)
+    {
+        if (m == n) return n;
+        return m + SumElements(m - 1, n);
+    }
+    return m;
+}
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
