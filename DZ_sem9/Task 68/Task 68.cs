34910acc5c0bfef6d3e84f7a1878d39428630997
RomanBusygin <roman.alex.95@mail.ru>
2022-07-24 15:29:58 +0500

    Функция Аккермана

---
+++ DZ_sem9/Task 68/Task 68.cs
@@ -0,0 +1,18 @@
+﻿int numberM = InputInt("Введите число M: ");
+int numberN = InputInt("Введите число N: ");
+
+Console.WriteLine($"Значение функции Аккермана для чисел {numberM} и {numberN} равно: {AckermanFunction(numberM, numberN)}");
+
+int AckermanFunction(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (m > 0 && n == 0) return AckermanFunction(m - 1, 1);
+    if (m > 0 && n > 0) return AckermanFunction(m - 1, AckermanFunction(m, n - 1));
+    return -1; // Если какое либо число отрицательное
+}
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
